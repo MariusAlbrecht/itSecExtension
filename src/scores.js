714bbb64c2fca162a235f9cf0d0665ddae20cc1f
Friedjof <friedjof@noweck.info>
2023-11-08 23:33:06 +0100

    fix: hash

--- src/scores.js
+++ src/scores.js
@@ -1,9 +1,13 @@
 var table = document.getElementsByTagName("tbody")[0]
 var headElem = document.createElement("th")
+
 headElem.style = "padding-left: 15px;"
 headElem.innerText = "score"
+
 document.getElementsByTagName("thead")[0].rows[0].appendChild(headElem)
+
 var score
+
 for (var i = 0, row; row = table.rows[i]; i++) {
     score = 0
     for (var j = 2, cell; cell = row.cells[j]; j++) {
