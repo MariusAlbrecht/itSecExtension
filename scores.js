224f40172276208454c655f2ba1628fc2684ebe2
Marius Albrecht <marius.albrecht@tum.de>
2023-11-09 23:03:30 +0100

    diesdas, kp mehr was ich alles gemacht hab

--- scores.js
+++ scores.js
@@ -1,17 +1,29 @@
-var table = document.getElementsByTagName("tbody")[0]
-var headElem = document.createElement("th")
+let table = document.getElementsByTagName("tbody")[0]
+let headElem = document.createElement("th")
+const addOnIncomplete = await isignoreInco() ? 0 : 9999
+const rowStart = await isIgnoreFirst4() ? 0 : 4
+
 headElem.style = "padding-left: 15px;"
 headElem.innerText = "score"
 document.getElementsByTagName("thead")[0].rows[0].appendChild(headElem)
-var score
-for (var i = 0, row; row = table.rows[i]; i++) {
+let score, mbyANumber
+for (let i = rowStart, row; row = table.rows[i]; i++) {
     score = 0
-    for (var j = 2, cell; cell = row.cells[j]; j++) {
-        var mbyANumber = parseInt(cell.title.split(".")[0])
-        score += isNaN(mbyANumber) ? 0 : mbyANumber
+    for (let j = 2, cell; cell = row.cells[j]; j++) {
+        mbyANumber = parseInt(cell.title.split(".")[0])
+        score += isNaN(mbyANumber) ? addOnIncomplete : mbyANumber
     }
-    var elem = document.createElement("td");
+    let elem = document.createElement("td");
     elem.style = "font-size: 1.3em; font-weight: bold; padding-left: 15px;"
     elem.innerText = score
     row.appendChild(elem)
+}
+
+async function isIgnoreFirst4() {
+    let res = await browser.storage.local.get('IgnoreFirst4')
+    return res.first4
+}
+async function isignoreInco() {
+    let res = await browser.storage.local.get('first4')
+    return res.first4
 }
